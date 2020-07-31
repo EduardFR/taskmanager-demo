a4e89982959b5c00e146a419af7e1634e9c28a99
Sasha Sushko <sushko@outlook.com>
2020-07-31 19:06:49 +0300

    2.5 Добавит логику отображения дедлайна
    Если "логике отношений" место в данных, то "логике отображение" - самое место в шаблоне.
    
    - Опишем функцию, которая будет проверять, просрочена ли задача
    - Используем её для добавления класса-модификатора

--- src/utils.js
+++ src/utils.js
@@ -8,4 +8,8 @@ function humanizeTaskDueDate(dueDate) {
   return dueDate ? dayjs(dueDate).format('D MMMM') : '';
 }
 
-export {getRandomArrayElement, humanizeTaskDueDate};
+function isTaskExpired(dueDate) {
+  return dueDate && dayjs().isAfter(dueDate, 'D');
+}
+
+export {getRandomArrayElement, humanizeTaskDueDate, isTaskExpired};
