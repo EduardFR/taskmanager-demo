b78f251ba5bc5cf5a00252aec8449b324ccc3490
Sasha Sushko <sushko@outlook.com>
2020-08-14 02:25:51 +0300

    6.2.2 Настроит flatpickr
    - Опишем обнуление в случае перерендеринга компонента
    - Добавим flatpickr на поле для выбора даты, если оно доступно для заполнения
    - Объявим обработчик ввода даты, чтобы записывал её в _state и обновлял элемент
    - Передадим обработчик в flatpickr

--- src/view/task-edit-view.js
+++ src/view/task-edit-view.js
@@ -146,6 +146,8 @@ function createTaskEditTemplate(data) {
 }
 
 export default class TaskEditView extends AbstractStatefulView {
+  #datepicker = null;
+
   constructor(task = BLANK_TASK) {
     super();
     this._setState(TaskEditView.parseTaskToState(task));
@@ -155,6 +157,17 @@ export default class TaskEditView extends AbstractStatefulView {
 
   get template() {
     return createTaskEditTemplate(this._state);
+  }
+
+  // Перегружаем метод родителя removeElement,
+  // чтобы при удалении удалялся более не нужный календарь
+  removeElement() {
+    super.removeElement();
+
+    if (this.#datepicker) {
+      this.#datepicker.destroy();
+      this.#datepicker = null;
+    }
   }
 
   reset(task) {
@@ -184,6 +197,12 @@ export default class TaskEditView extends AbstractStatefulView {
     evt.preventDefault();
     this._setState({
       description: evt.target.value,
+    });
+  };
+
+  #dueDateChangeHandler = ([userDate]) => {
+    this.updateElement({
+      dueDate: userDate,
     });
   };
 
@@ -218,6 +237,21 @@ export default class TaskEditView extends AbstractStatefulView {
       repeating: {...this._state.repeating, [evt.target.value]: evt.target.checked},
     });
   };
+
+  #setDatepicker() {
+    if (this._state.isDueDate) {
+      // flatpickr есть смысл инициализировать только в случае,
+      // если поле выбора даты доступно для заполнения
+      this.#datepicker = flatpickr(
+        this.element.querySelector('.card__date'),
+        {
+          dateFormat: 'j F',
+          defaultDate: this._state.dueDate,
+          onChange: this.#dueDateChangeHandler, // На событие flatpickr передаём наш колбэк
+        },
+      );
+    }
+  }
 
   #setInnerHandlers() {
     this.element.querySelector('.card__date-deadline-toggle')
