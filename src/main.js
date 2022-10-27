3f668868c25863f806ca22c1b2338b5b74aad275
Lydia Zakharova <jahontis@gmail.com>
2022-10-27 16:55:24 +0300

    Добавит модуль «Кнопка добавления задачи»
    - Создадим отдельный файл для компонента кнопки
    - Создадим класс NewTaskButtonView и функцию по созданию шаблона кнопки
    - Опишем в классе методы по получению шаблона и элемента кнопки
    - Импортируем класс кнопки в main.js и передадим экземпляр класса в рендер-функцию

--- src/main.js
+++ src/main.js
@@ -4,8 +4,3 @@ import TasksModel from './model/tasks-model.js';
 const tasksModel = new TasksModel({
   tasksApiService: new TasksApiService()
 });
-
-tasksModel.init()
-  .then(() => {
-    console.log(tasksModel.getTasks());
-  });
