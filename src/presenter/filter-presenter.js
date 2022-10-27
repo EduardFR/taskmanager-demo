c7052c64c3c1b878c30268a749791c0ec08cbafd
Lydia Zakharova <jahontis@gmail.com>
2022-10-27 17:49:09 +0300

    Добавит функции фильтрации

--- src/presenter/filter-presenter.js
+++ src/presenter/filter-presenter.js
@@ -1,11 +1,50 @@
 import FilterView from '../view/filter-view.js';
 import {render} from '../render.js';
+import {filter} from '../utils/filter.js';
+import {FilterType} from '../const.js';
 
 export default class FilterPresenter {
   constructor({filterContainer, filterModel, tasksModel}) {
     this.filterContainer = filterContainer;
     this.filterModel = filterModel;
     this.tasksModel = tasksModel;
+  }
+
+  getFilters() {
+    const tasks = this.tasksModel.getTasks();
+
+    return [
+      {
+        type: FilterType.ALL,
+        name: 'All',
+        count: filter[FilterType.ALL](tasks).length,
+      },
+      {
+        type: FilterType.OVERDUE,
+        name: 'Overdue',
+        count: filter[FilterType.OVERDUE](tasks).length,
+      },
+      {
+        type: FilterType.TODAY,
+        name: 'Today',
+        count: filter[FilterType.TODAY](tasks).length,
+      },
+      {
+        type: FilterType.FAVORITES,
+        name: 'Favorites',
+        count: filter[FilterType.FAVORITES](tasks).length,
+      },
+      {
+        type: FilterType.REPEATING,
+        name: 'Repeating',
+        count: filter[FilterType.REPEATING](tasks).length,
+      },
+      {
+        type: FilterType.ARCHIVE,
+        name: 'Archive',
+        count: filter[FilterType.ARCHIVE](tasks).length,
+      },
+    ];
   }
 
   init() {
