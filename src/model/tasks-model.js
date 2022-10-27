287707f08a40fd2b17b0ad693b2a8c978769b4db
Lydia Zakharova <jahontis@gmail.com>
2022-10-27 15:39:24 +0300

    Создаст модель задач

---
+++ src/model/tasks-model.js
@@ -0,0 +1,19 @@
+export default class TasksModel {
+  tasks = [];
+
+  getTasks() {
+    // Получить список задач
+  }
+
+  updateTask() {
+    // Обновить задачу
+  }
+
+  addTask() {
+    // Добавить задачу
+  }
+
+  deleteTask() {
+    // Удалить задачу
+  }
+}
