68fa8121ae255eb8cbab64b634420a5a16c3af05
Lydia Zakharova <jahontis@gmail.com>
2022-10-27 16:45:36 +0300

    Добавит модель фильтра

---
+++ src/model/filter-model.js
@@ -0,0 +1,13 @@
+import {FilterType} from '../const.js';
+
+export default class FilterModel {
+  filter = FilterType.ALL;
+
+  getFilter() {
+    return this.filter;
+  }
+
+  setFilter(filter) {
+    this.filter = filter;
+  }
+}
