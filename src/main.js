d6e3e981e4b2c7104baf8b4786e4199dfd31d90e
Lydia Zakharova <jahontis@gmail.com>
2022-10-27 16:58:58 +0300

    Добавит презентер доски
    - Создадим презентер - модуль, который вберёт в себя логику по работе с доской
    - Импортируем презентер в точку входа и инициализируем его

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import TasksModel from './model/tasks-model.js';
 import NewTaskButtonView from './view/new-task-button-view.js';
 import FilterView from './view/filter-view.js';
 import {render} from './render.js';
+import BoardPresenter from './presenter/board-presenter.js';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
@@ -10,6 +11,11 @@ const siteHeaderElement = siteMainElement.querySelector('.main__control');
 const tasksModel = new TasksModel({
   tasksApiService: new TasksApiService()
 });
+const boardPresenter = new BoardPresenter({
+  boardContainer: siteMainElement
+});
 
 render(new NewTaskButtonView(), siteHeaderElement);
 render(new FilterView(), siteMainElement);
+
+boardPresenter.init();
