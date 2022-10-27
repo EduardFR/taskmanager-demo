b55173ee355f9ba8adea6138c4e46512ce67f782
Lydia Zakharova <jahontis@gmail.com>
2022-10-27 16:56:39 +0300

    Разобьёт код на модули
    - Выделим остальные компоненты в отдельные файлы
    - Подключим компонент фильтра в точке входа

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import TasksApiService from './mock/tasks-api-service.js';
 import TasksModel from './model/tasks-model.js';
+import NewTaskButtonView from './view/new-task-button-view.js';
+import FilterView from './view/filter-view.js';
+import {render} from './render.js';
+
+const siteMainElement = document.querySelector('.main');
+const siteHeaderElement = siteMainElement.querySelector('.main__control');
 
 const tasksModel = new TasksModel({
   tasksApiService: new TasksApiService()
 });
+
+render(new NewTaskButtonView(), siteHeaderElement);
+render(new FilterView(), siteMainElement);
