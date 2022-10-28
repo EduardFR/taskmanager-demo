095f4336fa69185e740acfbd29cde8fce4ad6e6f
Lydia Zakharova <jahontis@gmail.com>
2022-10-28 15:08:14 +0300

    Добавит возможность переиспользования

--- src/presenter/task-presenter.js
+++ src/presenter/task-presenter.js
@@ -1,6 +1,11 @@
-import {render, replace} from '../framework/render.js';
+import {render, replace, remove} from '../framework/render.js';
 import TaskView from '../view/task-view.js';
 import TaskEditView from '../view/task-edit-view.js';
+
+const Mode = {
+  DEFAULT: 'DEFAULT',
+  EDITING: 'EDITING',
+};
 
 export default class TaskPresenter {
   #taskListContainer = null;
@@ -8,11 +13,16 @@ export default class TaskPresenter {
   #taskComponent = null;
   #taskEditComponent = null;
 
+  #mode = Mode.DEFAULT;
+
   constructor({taskListContainer}) {
     this.#taskListContainer = taskListContainer;
   }
 
   init(task) {
+    const prevTaskComponent = this.#taskComponent;
+    const prevTaskEditComponent = this.#taskEditComponent;
+
     this.#taskComponent = new TaskView({
       task,
       onEditClick: this.#handleEditClick,
@@ -22,7 +32,22 @@ export default class TaskPresenter {
       onFormSubmit: this.#handleFormSubmit,
     });
 
-    render(this.#taskComponent, this.#taskListContainer);
+    if (prevTaskComponent === null || prevTaskEditComponent === null) {
+      render(this.#taskComponent, this.#taskListContainer);
+      return;
+    }
+
+    if (this.#mode === Mode.DEFAULT) {
+      replace(this.#taskComponent, prevTaskComponent);
+    }
+
+    if (this.#mode === Mode.EDITING) {
+      replace(this.#taskComponent, prevTaskEditComponent);
+      this.#mode = Mode.DEFAULT;
+    }
+
+    remove(prevTaskComponent);
+    remove(prevTaskEditComponent);
   }
 
   #escKeyDownHandler = (evt) => {
@@ -43,10 +68,12 @@ export default class TaskPresenter {
   #replaceCardToForm = () => {
     replace(this.#taskEditComponent, this.#taskComponent);
     document.addEventListener('keydown', this.#escKeyDownHandler);
+    this.#mode = Mode.EDITING;
   };
 
   #replaceFormToCard = () => {
     replace(this.#taskComponent, this.#taskEditComponent);
     document.removeEventListener('keydown', this.#escKeyDownHandler);
+    this.#mode = Mode.DEFAULT;
   };
 }
