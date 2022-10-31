87d674dec166cd459c4af92a697d52ec49dbff8e
Lydia Zakharova <jahontis@gmail.com>
2022-10-31 11:52:14 +0300

    Реализует паттерн "Адаптер"

--- src/tasks-api-service.js
+++ src/tasks-api-service.js
@@ -8,19 +8,52 @@ const Method = {
 export default class TasksApiService extends ApiService {
   async getTasks() {
     return this._load({url: 'tasks'})
-      .then(ApiService.parseResponse);
+      .then(ApiService.parseResponse)
+      .then((tasks) => tasks.map(this.#adaptToClient));
   }
 
   async updateTask(task) {
     const response = await this._load({
       url: `tasks/${task.id}`,
       method: Method.PUT,
-      body: JSON.stringify(task),
+      body: JSON.stringify(this.#adaptToServer(task)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
 
-    return parsedResponse;
+    return this.#adaptToClient(parsedResponse);
+  }
+
+  #adaptToClient(task) {
+    const adaptedTask = {...task,
+      dueDate: task['due_date'] !== null ? new Date(task['due_date']) : task['due_date'],
+      isArchive: task['is_archived'],
+      isFavorite: task['is_favorite'],
+      repeating: task['repeating_days'],
+    };
+
+    delete adaptedTask['due_date'];
+    delete adaptedTask['is_archived'];
+    delete adaptedTask['is_favorite'];
+    delete adaptedTask['repeating_days'];
+
+    return adaptedTask;
+  }
+
+  #adaptToServer(task) {
+    const adaptedTask = {...task,
+      'due_date': task.dueDate instanceof Date ? task.dueDate.toISOString() : null,
+      'is_archived': task.isArchive,
+      'is_favorite': task.isFavorite,
+      'repeating_days': task.repeating,
+    };
+
+    delete adaptedTask.dueDate;
+    delete adaptedTask.isArchive;
+    delete adaptedTask.isFavorite;
+    delete adaptedTask.repeating;
+
+    return adaptedTask;
   }
 }
